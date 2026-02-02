a41c9426a95bbb8b5f4a17f68543d9ff2a060690
Mouheb Kraiem <mouheb.pro.work@gmail.com>
2026-02-02 14:44:08 +0100

    Rebrand to rendrio - add redirect and add icon

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect framio.io and www.framio.io to www.rendrio.io
+app.use((req, res, next) => {
+  const host = req.hostname;
+  if (host === 'framio.io' || host === 'www.framio.io') {
+    return res.redirect(301, 'https://www.rendrio.io' + req.originalUrl);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
