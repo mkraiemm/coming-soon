0382f51805a448dede9564dc443b2f8089469b46
Mouheb Kraiem <mouheb.pro.work@gmail.com>
2026-02-02 22:37:53 +0100

    Add video and wailist

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3050;
 
 // Redirect framio.io and www.framio.io to www.rendrio.io
 app.use((req, res, next) => {
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
